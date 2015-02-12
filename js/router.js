546f527306f3d3a9d8a2dab961184d99fba1a63e
isayme <isaymeorg@gmail.com>
2015-02-12 23:44:46 +0800

    add "#!" in url for disqus

--- js/router.js
+++ js/router.js
@@ -6,29 +6,53 @@ define([
 
   var Router = Backbone.Router.extend({
     routes: {
-      '': 'index',
-      ':title.html': 'post',
+      '(!/)': 'index',
+      '(!/):title.html': 'post',
       '*defAction': 'defAction'
     },
 
     index: function() {
+      var self = this;
       require([
         'views/index'
       ], function(indexView) {
         indexView.render();
+        self.reloadDisqus();
       });
     },
 
     post: function(title) {
+      var self = this;
       require([
         'views/index'
       ], function(indexView) {
         indexView.render(title);
+        self.reloadDisqus(title);
       });
     },
 
     defAction: function(permalink) {
       console.log('[router] 404: ' + permalink);
+    },
+
+    reloadDisqus: function(identifier) {
+      window.disqus_identifier = identifier;
+
+      if (window.DISQUS) {
+        window.DISQUS.reset({
+          reload: true,
+          config: function () {  
+            this.page.identifier = window.disqus_identifier;
+          }
+        });
+      } else {
+        (function() {
+          var disqus_shortname = 'cainixiangbudao';
+          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+          dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+      }
     }
   });
 
